24872267d290418afb2ce1a6f2dc1d40366f2869
BryceFuerty <95210822+BryceFuerty@users.noreply.github.com>
2024-02-19 21:00:03 +0100

    fix

--- website-pepino/tailwind.config.js
+++ website-pepino/tailwind.config.js
@@ -12,10 +12,8 @@ module.exports = {
     },
   },
   plugins: [
-    require('postcss-import'),
-    require('tailwindcss/nesting')(require('postcss-nesting')),
-    require('autoprefixer'),
-    require('tailwindcss'),
+   (require('postcss-nesting')),
+
   ],
 }
 
